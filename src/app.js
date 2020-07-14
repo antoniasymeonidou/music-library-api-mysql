9b8134ec1fefda686f88963b13dd10fd24486b1a
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-07-14 20:33:48 +0100

    get all artists

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
+const artistControllers = require('./controllers/artist');
 
 const app = express();
 
 app.use(express.json());
+app.post('/artist', artistControllers.create);
+app.get('/artist', artistControllers.list);
 
 module.exports = app;
