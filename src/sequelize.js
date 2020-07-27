c4908a3a8868427d04571350923ad94b45741da8
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-07-27 18:21:17 +0100

    Fixed db

--- src/sequelize.js
+++ src/sequelize.js
@@ -4,15 +4,16 @@ const AlbumModel = require('./models/album');
 const SongModel = require('./models/song');
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, CLEARDB_DATABASE_URL } = process.env;
-
-const sequelize = CLEARDB_DATABASE_URL ?
-  new Sequelize(CLEARDB_DATABASE_URL) :
-  new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+const setupDatabase = () => {
+  const sequelize = CLEARDB_DATABASE_URL ?
+    new Sequelize(CLEARDB_DATABASE_URL) :
+    new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
       host: DB_HOST,
       port: DB_PORT,
       dialect: 'mysql',
       logging: false,
     });
+
 
   const Artist = ArtistModel(sequelize, Sequelize);
   const Album = AlbumModel(sequelize, Sequelize);
@@ -27,6 +28,7 @@ const sequelize = CLEARDB_DATABASE_URL ?
     Artist,
     Album,
     Song,
-  };
+  }
+};
 
 module.exports = setupDatabase();
